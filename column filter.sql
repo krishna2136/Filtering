fb33c2cafee926d31f18605a732f5acd7e048dba
Krishna Reddy <67143205+krishna2136@users.noreply.github.com>
2020-08-28 23:31:00 +0530

    Update column filter.sql

--- column filter.sql
+++ column filter.sql
@@ -9,15 +9,15 @@ VALUES
     ('CANADA', 'TORONTO', 3, 'UNKNOWN', 3, 'IT', 1),
     ('AUSI', 'SYDNEY', 1, 'UNKNOWN1', 1, 'IT-2', 2),
     ('SINGAPORE', 'MALAYSIA', 2, 'UNKNOWN2', 3, 'FANTASTIC BEASTS', 2),
-    ('RUSSIA', 'MOSCOW', 3, 'UNKNOWN3', 1, 'JUNGLE BOOK', 1),
-    ('SRILANKA', 'COLOMBO', 1, 'JACQUELINE', 2, 'ROY', 3)
+    ('RUSSIA', 'MOSCOW', 1, 'UNKNOWN3', 2, 'JUNGLE BOOK', 3),
+    ('INDIA', 'VIZAG', 3, 'JACQUELINE', 1, 'ROY', 1)
 ;
 
 
 
 
 With unpivot_me As (
-SELECT Country, 'City' as category, City as cat_value, City_Rank as rank_value
+SELECT Country, 'City' as category, y as cat_value, City_Rank as rank_value
 FROM Table1
 UNION ALL
 SELECT Country, 'Artist' as category, Artist as cat_value, Artist_Rank as rank_value
